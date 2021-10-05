baabf30f49188eb7b1c1a2eeb0e93b3f337ab0b5
Xavier <xavier.coello@epn.edu.ec>
2021-10-04 21:00:19 -0500

    Primer commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    output: {
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                   // minimize: false,
+                },
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles.css$/,
+                use: [MiniCssExtract.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+            }
+
+        ]
+    },
+    optimization: {}, 
+    plugins: [
+        new HtmlWebPackPlugin({           
+            //filename: './index.html',
+            template: './src/index.html',
+            // //inject: 'body'
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+         new CopyPlugin({
+             patterns: [
+                 { from: 'src/assets/', to: 'assets/' }
+             ]
+         })
+    ]
+}
